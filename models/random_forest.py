d8fb2612649c8c1a5ce9e46fe50e52c9ff6c44e2
Elizabeth-Flx <daniel.lochert.flx@gmail.com>
2026-01-22 17:05:42 +0000

    added basic code structure to simple models

---
+++ models/random_forest.py
@@ -0,0 +1,21 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import lib.dataloader as dl
+import lib.datasplit as ds
+import lib.metrics as me
+import encoding.kmer_freq as kf
+import pandas as pd
+import numpy as np
+
+
+df = dl.load_airr_dataset("simulated_2k_balanced_noisy_25_dataset")
+df = kf.encode_repertorie_normalized(df, k=3, sequence_column="cdr3_aa", sample_column="sample", label_column="disease")
+
+print (f"Encoded {len(df)} samples with k-mer frequencies.")
+
+train, test = ds.split_data(df)
+
+print(train)
+print(test)

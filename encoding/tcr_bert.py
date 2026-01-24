4f7a402c4c529ec40a28f3282cae57f02838ff6a
Elizabeth-Flx <daniel.lochert.flx@gmail.com>
2026-01-24 17:01:32 +0000

    added encode_repertorie function

--- encoding/tcr_bert.py
+++ encoding/tcr_bert.py
@@ -4,8 +4,14 @@ from transformers import BertTokenizer
 import torch
 import torch.nn.functional as F
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 model = BertModel.from_pretrained("wukevin/tcr-bert")
 tokenizer = BertTokenizer.from_pretrained("wukevin/tcr-bert")
+
+model.to(device)
+
+print(f"TCR-BERT model loaded on device: {device}")
 
 def tokeninze_tcrs(sequences, max_length=64):
     # space separate the sequences
@@ -28,8 +34,8 @@ def tokeninze_tcrs(sequences, max_length=64):
         
     return inputs
 
-def generate_tcr_embeddings(sequences, max_length=64):
-    inputs = tokeninze_tcrs(sequences, max_length=max_length)
+def generate_tcr_embeddings(sequences, max_length=64, device=device):
+    inputs = tokeninze_tcrs(sequences, max_length=max_length).to(device)
     with torch.no_grad():
         outputs = model(**inputs, output_hidden_states=True)
     # get the last hidden states
@@ -55,3 +61,21 @@ def aggregate_tcr_embeddings(embedding, tcr_agg_method="mean", repertoire_agg_me
         raise ValueError(f"Unknown repertoire_agg_method: {repertoire_agg_method}")
 
     return repertoire_embedding
+
+def encode_repertorie(
+    df,
+    sequence_col="cdr3_aa",
+    max_length=64,
+    tcr_agg_method="mean",
+    repertoire_agg_method="mean",
+    device=device,
+):
+    df["embeddings"] = df.apply(
+            lambda row: aggregate_tcr_embeddings(
+                generate_tcr_embeddings(row[sequence_col], max_length=max_length, device=device),
+                tcr_agg_method=tcr_agg_method,
+                repertoire_agg_method=repertoire_agg_method,
+        ),
+        axis=1
+    )
+    return df

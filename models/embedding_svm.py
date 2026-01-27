f2e6990a9ffef06219b00ade090d649b468296e8
Elizabeth-Flx <daniel.lochert.flx@gmail.com>
2026-01-27 14:37:57 +0000

    now uses subset of data for testing

--- models/embedding_svm.py
+++ models/embedding_svm.py
@@ -10,30 +10,37 @@ import encoding.tcr_bert as tcrbert
 import pandas as pd
 import numpy as np
 
+np.set_printoptions(threshold=10000)
 
-if not os.path.exists("./data/simulated_2k_balanced_noisy_25_dataset"):
+
+if not os.path.exists("./data/dataset_7"):
     print("Computing TCR-BERT embeddings...")
     
-    df = dl.load_airr_dataset("simulated_2k_balanced_noisy_25_dataset")
-    df_merged = dl.merge_dataset(df)
-
-    df_embedding = tcrbert.encode_repertorie(
-        df_merged,
-        sequence_col="cdr3_aa",
-        max_length=64,
-        tcr_agg_method="mean",
-        repertoire_agg_method="mean"
-    )
-
-    print (f"Encoded {len(df_embedding)} samples with TCR-BERT embeddings.")
-    print(df_embedding.head())
+    df = dl.load_kaggle_dataset("dataset_7")
     
-    os.makedirs("./data/simulated_2k_balanced_noisy_25_dataset", exist_ok=True)
-    df_embedding.to_pickle("./data/simulated_2k_balanced_noisy_25_dataset/df_embedding.pkl")
+    
+    # for testing only use 1000th of the data
+    df = df.sample(frac=0.01, random_state=42).reset_index(drop=True)
+    
+    # save testing dataframe
+    df.to_csv("./df_sampled.csv", index=False)
+    
+    # load data
+    df = pd.read_csv("./df_sampled.csv")
+    
+    input_ids, attention_masks, embeddings = tcrbert.encode_sequences(df, seq_col="junction_aa", max_length=32, batch_size=int(8192/2))
+    
+    # save as npz file
+    np.savez_compressed(
+        "./embeddings_tcrbert.npz",
+        input_ids=input_ids,
+        attention_masks=attention_masks,
+        embeddings=embeddings
+    )
+ 
+    
 else:
-    df_embedding = pd.read_pickle("./data/simulated_2k_balanced_noisy_25_dataset/df_embedding.pkl")
-    print("Loaded precomputed embeddings.")
-    print(df_embedding.head())
+    pass
 
 
 import numpy as np
@@ -43,6 +50,52 @@ from sklearn.svm import SVC
 # from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+
+
+from scipy.spatial.distance import pdist, squareform
+
+
+print(input_ids.shape)
+print(input_ids)
+
+print(input_ids[0])
+print(input_ids[-1])
+
+print(attention_masks.shape)
+print(attention_masks)
+
+print(embeddings.shape)
+print(embeddings)
+
+dist_matrix = squareform(pdist(embeddings*10000, metric="cosine"))
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(6, 5))
+plt.imshow(dist_matrix)
+plt.colorbar(label="Distance")
+plt.title("Pairwise Embedding Distances")
+plt.xlabel("Sample index")
+plt.ylabel("Sample index")
+plt.tight_layout()
+# save figure
+plt.savefig("pairwise_embedding_distances.png", dpi=300)
+# plt.show()
+
+# plot histogram of distances
+# log scale y axis
+plt.figure(figsize=(6, 4))
+plt.hist(dist_matrix.flatten(), bins=80, color='blue', alpha=0.7, edgecolor='black')
+plt.title("Histogram of Pairwise Embedding Distances")
+plt.xlabel("Distance")
+# plt.xscale("log")
+plt.ylabel("Frequency")
+plt.yscale("log")
+plt.tight_layout()
+plt.savefig("histogram_embedding_distances.png", dpi=300)
+# plt.show()
+
+
 
 
 # X = np.vstack(df_embedding["embeddings"].values)

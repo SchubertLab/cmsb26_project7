c2d64370a250aae9167530ed4beda4198abc349e
Elizabeth-Flx <daniel.lochert.flx@gmail.com>
2026-02-16 15:46:53 +0000

    gnn trains, but very badly

--- models/gnn/tcrGNN.py
+++ models/gnn/tcrGNN.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GINConv, global_mean_pool
-
-
+from torch_geometric.nn import GINConv, GINEConv, global_mean_pool
 
 class TCRgnn(torch.nn.Module):
     
@@ -31,7 +29,7 @@ class TCRgnn(torch.nn.Module):
             torch.nn.Linear(hidden_dim, hidden_dim),
             torch.nn.ReLU(),
             torch.nn.Dropout(dropout),
-            torch.nn.Linear(hidden_dim, num_classes),
+            torch.nn.Linear(hidden_dim, 1),
         )
         
     def forward(self, data):
@@ -42,6 +40,63 @@ class TCRgnn(torch.nn.Module):
         x = F.dropout(x, p=self.dropout, training=self.training)
         
         x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        
+        x = global_mean_pool(x, batch)
+        
+        out = self.classifier(x)
+        
+        return out
+    
+
+class TCRgnnEdge(torch.nn.Module):
+    
+    def __init__(self, in_dim, hidden_dim, num_classes, edge_dim=1, dropout=0.3):
+        super().__init__()
+        
+        self.dropout = dropout
+        
+        self.conv1 = GINEConv(
+            torch.nn.Sequential(
+                torch.nn.Linear(in_dim, hidden_dim),
+                torch.nn.ReLU(),
+                torch.nn.Linear(hidden_dim, hidden_dim),
+            ),
+            edge_dim=edge_dim
+        )
+        
+        self.conv2 = GINEConv(
+            torch.nn.Sequential(
+                torch.nn.Linear(hidden_dim, hidden_dim),
+                torch.nn.ReLU(),
+                torch.nn.Linear(hidden_dim, hidden_dim),
+            ),
+            edge_dim=edge_dim
+        )
+        
+        
+        
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(dropout),
+            torch.nn.Linear(hidden_dim, 1),
+        )
+        
+    def forward(self, data):
+        x, edge_index, edge_attr, batch = (
+            data.x, 
+            data.edge_index, 
+            data.edge_attr, 
+            data.batch
+        )
+        
+        x = self.conv1(x, edge_index, edge_attr)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        
+        x = self.conv2(x, edge_index, edge_attr)
         x = F.relu(x)
         x = F.dropout(x, p=self.dropout, training=self.training)
         
